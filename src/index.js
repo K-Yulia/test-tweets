fd505e27325609175c3a1eb825d97bf9ddaa8ad1
Yulia Kutova <kutova85@gmail.com>
2023-04-28 23:04:06 +0300

    add

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/test-tweets">
+        <BrowserRouter basename="test-tweets">
           <App />
         </BrowserRouter>
       </PersistGate>
